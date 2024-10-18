f7d858b8d23c0d022f24ae0350e12b80a1470177
Rohit <167241336+rohitcreates@users.noreply.github.com>
2024-10-18 17:11:32 +0000

    shopping website

--- Wesbite/assets/JavaScript/script.js
+++ Wesbite/assets/JavaScript/script.js
@@ -3,8 +3,6 @@ const buyButton = document.querySelector('.buy-btn')
  buyButton.addEventListener('click', function () {
      const buyButtonValue = document.querySelector('.buy-btn span').innerHTML
      const cartButton = document.querySelector('.cart-btn span').innerHTML = buyButtonValue
-    console.log(cartButton);
-    console.log(buyButtonValue);
  })
 
 
