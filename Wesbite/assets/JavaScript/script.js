81a81a5798090939a7ca83b4fcd106c0644c3ddd
Rohit <167241336+rohitcreates@users.noreply.github.com>
2024-10-19 06:59:04 +0000

    shopping website

--- Wesbite/assets/JavaScript/script.js
+++ Wesbite/assets/JavaScript/script.js
@@ -1,32 +1,46 @@
-const buyButton = document.querySelector('.buy-btn')
+const buyButton = document.querySelector(".buy-btn");
 
- buyButton.addEventListener('click', function () {
-     const buyButtonValue = document.querySelector('.buy-btn span').innerHTML
-     const cartButton = document.querySelector('.cart-btn span').innerHTML = buyButtonValue
- })
+buyButton.addEventListener("click", function () {
+  const buyButtonValue = document.querySelector(".buy-btn span").innerHTML;
+  const cartButton = (document.querySelector(".cart-btn span").innerHTML =
+    buyButtonValue);
+});
 
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
 
-const nextBtn = document.querySelector('.next-btn')
-const prevBtn = document.querySelector('.prev-btn')
+nextBtn.addEventListener("click", function () {
+  let products = document.querySelectorAll(".product");
+  document.querySelector(".product-content").appendChild(products[0]);
+});
 
-nextBtn.addEventListener('click', function () {
-    let products = document.querySelectorAll('.product')
-    document.querySelector('.product-content').appendChild(products[0])
-})
+prevBtn.addEventListener("click", function () {
+  let products = document.querySelectorAll(".product");
+  document
+    .querySelector(".product-content")
+    .prepend(products[products.length - 1]);
+});
 
-prevBtn.addEventListener('click', function () {
-    let products = document.querySelectorAll('.product')
-    document.querySelector('.product-content').prepend(products[products.length - 1])
-})
+// const icon_1 = document.querySelector('.icon-1');
 
-const icon = document.querySelector('.icon');
+// const hoverItem = document.querySelector('.hover-item')
 
-const hoverItem = document.querySelector('.hover-item')
+// icon_1.addEventListener('mouseover', function () {
+//     hoverItem.style.display = "flex"
+// })
+
+// icon_1.addEventListener('mouseout', function () {
+//    hoverItem.style.display = "none"
+// })
+
+const icon = document.querySelector("icon-2");
+const hoverItem = document.querySelector(".hover-item");
 
 icon.addEventListener('mouseover', function () {
     hoverItem.style.display = "flex"
-}) 
+})
+
 
 icon.addEventListener('mouseout', function () {
-   hoverItem.style.display = "none" 
+    hoverItem.style.display = "none"
 })
